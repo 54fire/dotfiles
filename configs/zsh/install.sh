536dedbc6003513c2e55e0d4dbe84eba156f429f
54fire <54fireljw@gmail.com>
2025-05-14 10:12:09 +0800

    修改zsh插件安装

--- configs/zsh/install.sh
+++ configs/zsh/install.sh
@@ -16,3 +16,25 @@ install_zsh() {
     sudo apt-get install -y zsh
   fi
 }
+
+install_plugin() {
+  local repo_url=$1
+  local plugin_name=$(basename -s .git "$repo_url")
+  local plugin_path="${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/${plugin_name}"
+
+  if [ ! -d "$plugin_path" ]; then
+    echo "Installing $plugin_name..."
+    git clone "$repo_url" "$plugin_path"
+  else
+    echo "$plugin_name already installed."
+  fi
+}
+
+# install plugins
+install_plugin "https://github.com/zsh-users/zsh-autosuggestions.git"
+install_plugin "https://github.com/zsh-users/zsh-syntax-highlighting.git"
+
+if ! command -v fzf &> /dev/null; then
+  sudo apt install fzf -y
+fi
+install_plugin "https://github.com/Aloxaf/fzf-tab.git"
